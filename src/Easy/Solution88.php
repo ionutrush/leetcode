0af0ef4baa7f854b4df67ad3f3c8a50996eb2a51
Ionut Rus <pixelangello@gmail.com>
2025-04-23 13:23:16 +0300

    Refactor merge function to simplify logic.
    Replaced manual zero filtering and array splicing with `array_slice` for clarity. Simplified length normalization by using `array_pad` directly after sorting. This improves code readability and reduces unnecessary operations. Memory usage improved.

--- src/Easy/Solution88.php
+++ src/Easy/Solution88.php
@@ -14,21 +14,12 @@ class Solution88 extends Solution
      * @return void
      */
     function merge(array &$nums1, int $m, array $nums2, int $n): void {
-        $targetLength = $m + $n;
-        $length = $targetLength;
-        $targetIndex = $targetLength - 1;
-
         // filter 0s out of nums1, but only from the end until we hit a number different to 0
-        while ($targetIndex >= 0 && $nums1[$targetIndex] === 0) {
-            $targetIndex--;
-            array_splice($nums1, $targetIndex + 1, 1);
-        }
+        $nums1 = array_slice($nums1, 0, $m);
 
         $nums1 = array_merge($nums1, $nums2);
         sort($nums1);
-        if (count($nums1) < $length) {
-            $nums1 = array_pad($nums1, $length, 0);
-        }
+        $nums1 = array_pad($nums1, $m + $n, 0);
     }
 
     public function run(...$args): void
