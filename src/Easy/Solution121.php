ddc0f125555a39c79bc895c181381fb2c4a60fa4
Ionut Rus <pixelangello@gmail.com>
2025-04-29 18:30:03 +0300

    Optimize maxProfit calculation by tracking min price.
    Replaced nested loops with a single loop for improved efficiency. Introduced a variable to track the minimum price dynamically, reducing time complexity from O(n^2) to O(n).

--- src/Easy/Solution121.php
+++ src/Easy/Solution121.php
@@ -12,10 +12,23 @@ class Solution121 extends Solution
      */
     function maxProfit(array $prices): int {
         $maxProfit = 0;
-        foreach ($prices as $i => $price) {
-            for ($j = $i + 1; $j < count($prices); $j++) {
-                $maxProfit = max($maxProfit, $prices[$j] - $price);
+        $minPrice = $prices[0];
+
+        for ($i = 0; $i < count($prices); $i++) {
+            if (isset($minPrice)) {
+                $maxProfit = max($maxProfit, $prices[$i] - $minPrice);
             }
+
+            if ($i === count($prices) - 1) {
+                break;
+            }
+
+            $minPrice = (
+                $i === 0 ||
+                $prices[$i] < $prices[$i - 1]
+            )
+                ? min($minPrice, $prices[$i])
+                : min($minPrice, $prices[$i - 1]);
         }
 
         return $maxProfit;
