77e5611dbb985f18fa1dcb79f50619d85f507f24
Ionut Rus <pixelangello@gmail.com>
2025-04-29 16:34:39 +0300

    Add unit tests for Solution118 implementation
    Introduce PHPUnit tests for Solution118, including examples for generating Pascal's Triangle with different input sizes. These tests ensure the correctness of the `generate` method's output.

---
+++ tests/Solution118Test.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Rushdevelopment\Leetcode\Tests;
+
+require "vendor/autoload.php";
+
+use PHPUnit\Framework\TestCase;
+use Rushdevelopment\Leetcode\Easy\Solution118;
+use Rushdevelopment\Leetcode\ListNode;
+use Rushdevelopment\Leetcode\SolutionInterface;
+use Rushdevelopment\Leetcode\TreeNode;
+
+class Solution118Test extends TestCase
+{
+    private ?SolutionInterface $solution;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->solution = new Solution118();
+    }
+
+   public function testGenerateExample1(): void
+   {
+       $this->assertEquals(
+           [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]],
+           $this->solution->generate(5)
+       );
+   }
+
+   public function testGenerateExample2(): void
+   {
+       $this->assertEquals(
+           [[1]],
+           $this->solution->generate(1)
+       );
+   }
+}
