3f2034b8a3e4c7e1aef78a9a1c5c5bece4c26e3e
Ionut Rus <pixelangello@gmail.com>
2025-04-30 12:41:26 +0300

    Implement majority element identification in Solution169
    Replaced the hardcoded return value with logic to determine the majority element from the input array. Added a check using array_count_values and floor to handle the element count comparison. Throws an exception if no majority element is found, ensuring input validity.
    
    This first implementation already performs very well in runtime.

--- src/Easy/Solution169.php
+++ src/Easy/Solution169.php
@@ -7,11 +7,20 @@ use Rushdevelopment\Leetcode\Solution;
 class Solution169 extends Solution
 {
     /**
+     * Given an array nums of size n, return the majority element.
+     * The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
      * @param Integer[] $nums
      * @return Integer
      */
     function majorityElement(array $nums): int {
-        return 3;
+        $minSize = floor(count($nums) / 2);
+        foreach (array_count_values($nums) as $value => $count) {
+            if ($count > $minSize) {
+                return $value;
+            }
+        }
+
+        throw new \InvalidArgumentException('No majority element found');
     }
     public function run(...$args): int
     {
